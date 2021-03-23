40a11cee00abfba3f79e2b26469a53690f9612df
Jack Lin <majic0626@gmail.com>
2021-03-23 21:06:19 +0800

    deleted used files and rename detect.py

--- test_cifar10.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python3 rotate_detect.py --method MSP --num_eval 5
-python3 rotate_detect.py --method MaxMax --num_eval 5
-python3 rotate_detect.py --method MeanMax --num_eval 5
-python3 rotate_detect.py --method MeanPosMax --num_eval 5
-python3 rotate_detect.py --method JSD --num_eval 5
